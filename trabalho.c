107004e6d8b1d3c91e84c7b8a9c28c260ad20c88
Gabrielarnaud01 <105997305+Gabrielarnaud01@users.noreply.github.com>
2022-07-20 10:15:28 -0300

    trazendo o codigo do fork-clone

---
+++ trabalho.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "struct.h"
+#include "functions.h"
+
+
+int main() {
+  // matriz do jogo
+  espaco **matriz;
+  int *vet_rand;
+  menu(matriz, vet_rand);
+
+  return 0;
+}
