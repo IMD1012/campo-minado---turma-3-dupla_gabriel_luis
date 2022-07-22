e0b3a4c1875d8b87035ba6c529b738db69f39b53
Luis Candido <103050923+luiseduardofc1500@users.noreply.github.com>
2022-07-22 03:26:00 -0300

    Joguei o manual para o main para fazer a manipulação de arquivos

--- trabalho.c
+++ trabalho.c
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,9 +8,15 @@
 
 int main() {
   // matriz do jogo
+  FILE *manual;
+  char texto_manual[800];
   espaco **matriz;
   int *vet_rand;
+  manual = fopen("instrucoes.txt", "r");
+  while(fgets(texto_manual,700,manual)!= NULL){
+    printf("%s", texto_manual);
+  }
+  fclose(manual);
   menu(matriz, vet_rand);
-
   return 0;
 }
